7d2fe1e6a28745779537b18cad73136cdd806849
dayou <dayou@weierai.com>
2021-07-12 14:37:53 +0800

    余杭多区域租房分析

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import os
 import sys
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-execute(['scrapy', 'crawl', 'lianjiazufang', '-o', 'lianjiazufang5.csv'])
+execute(['scrapy', 'crawl', 'lianjiazufang', '-o', 'lianjiazufang6.csv'])
